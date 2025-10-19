4ba0114c8e11cc78f6d54fdbacf0857dd963833d
piyush-mit <piyushmit5758@gmail.com>
2025-10-19 16:05:29 +0530

    prisma fixes and syncUser action

--- konnekt/components/navbar.tsx
+++ konnekt/components/navbar.tsx
@@ -2,8 +2,14 @@ import Link from 'next/link';
 import React from 'react'
 import DesktopNavbar from './DesktopNavBar';
 import MobileNavbar from './MobileNavbar';
+import { currentUser } from '@clerk/nextjs/server';
+import { syncUser } from '@/actions/user.action';
 
-function NavBar() {
+async function NavBar() {
+  
+  const user = await currentUser();
+  // is user is signed in make sure he is synced in our database
+  if(user) await syncUser(); // server action  (POST)
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
