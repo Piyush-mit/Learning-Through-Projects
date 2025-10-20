0902a690192d07d13b5e6ab8008499cc8e561606
piyush-mit <piyushmit5758@gmail.com>
2025-10-20 16:55:40 +0530

    added follow feature

--- konnekt/app/page.tsx
+++ konnekt/app/page.tsx
@@ -2,18 +2,18 @@
 import React from 'react'
 import { currentUser } from '@clerk/nextjs/server'
 import CreatePost from '@/components/CreatePost';
+import WhoToFollow from '@/components/Suggestions';
 
 async function Home() {
   const user = await currentUser();
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-10'>
+    <div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
       <div className='lg:col-span-6'>
           {user ? <CreatePost/> : null}
       </div>
       <div className='hidden lg:block lg:col=span-4 sticky top-20'>
-        Find User
+        <WhoToFollow/>
       </div>
-      
     </div>
   )
 }
